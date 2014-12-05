cc5d64726f957ad8b607a9ae54244214300b26fc
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook-Pro.local>
2014-12-05 19:08:52 -0200

    Initial Commit

---
+++ controle-cliente/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  controle-cliente
+//
+//  Created by Macbook Pro on 05/12/14.
+//  Copyright (c) 2014 CSI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
